605f5ef629d79067e02863e30216b959874e2fbb
valmira.shala <valmira.shala@life.gjirafa.com>
2023-04-04 11:29:39 +0200

    Add dependency injection

---
+++ RecipeSharingApi/RecipeSharingApi.BusinessLogic/Helpers/StartupHelper.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using RecipeSharingApi.BusinessLogic.Services;
+using RecipeSharingApi.BusinessLogic.Services.IServices;
+using RecipeSharingApi.Data.Data.UnitOfWork;
+using RecipeSharingApi.DataLayer.Data.UnitOfWork;
+
+namespace RecipeSharingApi.BusinessLogic.Helpers {
+    public static class StartupHelper
+    {       
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddTransient<IRecipeService, RecipeService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();    
+            services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+    
+        }
+    }
+}
--- RecipeSharingApi/RecipeSharingApi/Program.cs
+++ RecipeSharingApi/RecipeSharingApi/Program.cs
@@ -1,14 +1,18 @@
 using Microsoft.EntityFrameworkCore;
+using RecipeSharingApi.BusinessLogic.Services.IServices;
+using RecipeSharingApi.BusinessLogic.Services;
 using RecipeSharingApi.DataLayer.Data;
-
+using RecipeSharingApi.DataLayer.Data.UnitOfWork;
+using RecipeSharingApi.Data.Data.UnitOfWork;
+using RecipeSharingApi.DataLayer.Data.Repository.IRepository;
+using RecipeSharingApi.DataLayer.Models.Entities;
+using RecipeSharingApi.DataLayer.Data.Repository;
+using AutoMapper;
+using RecipeSharingApi.BusinessLogic.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddDbContext<RecipeSharingDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("RecipeSharingConnection")));
-
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,8 +20,20 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
 
+builder.Services.AddDbContext<RecipeSharingDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("RecipeSharingConnection")));
+
+
+var mapperConfiguration = new MapperConfiguration(
+    mc => mc.AddProfile(new AutoMapperConfigurations()));
+IMapper mapper = mapperConfiguration.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+builder.Services.AddServices();
+
+
+var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
