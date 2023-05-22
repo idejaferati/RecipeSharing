a82fbddff870ce91dcd092255753141e3eb36b53
Valmira Shala <93986564+valmirashala@users.noreply.github.com>
2023-05-23 00:45:15 +0200

    add swagger documentation for recipe and cookbook controller

--- RecipeSharingApi/RecipeSharingApi/Controllers/CookBookController.cs
+++ RecipeSharingApi/RecipeSharingApi/Controllers/CookBookController.cs
@@ -4,151 +4,210 @@ using RecipeSharingApi.BusinessLogic.Services;
 using RecipeSharingApi.BusinessLogic.Services.IServices;
 using RecipeSharingApi.DataLayer.Models.DTOs.CookBook;
 using RecipeSharingApi.DataLayer.Models.Entities;
-using System.Security.Claims;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace RecipeSharingApi.Controllers;
-[ApiController]
-[Route("api/cookbooks")]
-public class CookBookController : ControllerBase
+namespace RecipeSharingApi.Controllers
 {
-    private readonly ICookBookService _cookBookService;
-    private readonly IUserService _userService;
-
-
-    public CookBookController(ICookBookService cookBookService, IUserService userService)
+    [ApiController]
+    [Route("api/cookbooks")]
+    public class CookBookController : ControllerBase
     {
-        _cookBookService = cookBookService;
-        _userService = userService;
+        private readonly ICookBookService _cookBookService;
+        private readonly IUserService _userService;
 
-    }
-
-    [HttpPost]
-    [Authorize(Policy= "userPolicy")]
-    public async Task<ActionResult<CookBookDTO>> Create(CookBookCreateRequestDTO cookBookToCreate)
-    {
-        try
+        public CookBookController(ICookBookService cookBookService, IUserService userService)
         {
-            var UserId = _userService.GetMyId();
+            _cookBookService = cookBookService;
+            _userService = userService;
+        }
+
+        /// <summary>
+        /// Creates a new cookbook.
+        /// </summary>
+        /// <param name="cookBookToCreate">The cookbook data to create.</param>
+        /// <returns>The created cookbook.</returns>
+        [HttpPost]
+        [Authorize(Policy = "userPolicy")]
+        [ProducesResponseType(typeof(CookBookDTO), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        public async Task<ActionResult<CookBookDTO>> Create(CookBookCreateRequestDTO cookBookToCreate)
+        {
+            try
+            {
+            var userId = _userService.GetMyId();
             CookBookDTO cookBook;
             
-            cookBook = await _cookBookService.Create(cookBookToCreate, UserId);
+            cookBook = await _cookBookService.Create(cookBookToCreate, userId);
             
-            return Ok(cookBook);
+                return Ok(cookBook);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
-        catch (Exception ex) 
-        { 
-            return BadRequest(ex.Message); 
-        }
-    }
 
-    [HttpGet("all")]
-    [Authorize(Policy = "adminPolicy")]
-    public async Task<ActionResult<List<CookBookDTO>>> GetAll()
-    {
-        try
+        /// <summary>
+        /// Retrieves all cookbooks.
+        /// </summary>
+        /// <returns>The list of all cookbooks.</returns>
+        [HttpGet("all")]
+        [Authorize(Policy = "adminPolicy")]
+        [ProducesResponseType(typeof(List<CookBookDTO>), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        public async Task<ActionResult<List<CookBookDTO>>> GetAll()
         {
-            var cookBooks = await _cookBookService.GetAll();
-            return Ok(cookBooks);
+            try
+            {
+                var cookBooks = await _cookBookService.GetAll();
+                return Ok(cookBooks);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
-        catch (Exception ex) 
-        { 
-            return NotFound(ex.Message); 
-        }
-    }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<CookBookDTO>> Get(Guid id)
-    {
-        try
+        /// <summary>
+        /// Retrieves a specific cookbook by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the cookbook to retrieve.</param>
+        /// <returns>The cookbook with the specified ID.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(CookBookDTO), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        public async Task<ActionResult<CookBookDTO>> Get(Guid id)
         {
-            var cookBook = await _cookBookService.Get(id);
-            return Ok(cookBook);
+            try
+            {
+                var cookBook = await _cookBookService.Get(id);
+                return Ok(cookBook);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
-        catch (Exception ex) 
-        { 
-            return NotFound(ex.Message); 
-        }
-    }
 
-    [HttpGet]
-    public async Task<ActionResult<List<CookBookDTO>>> GetPaginated(int page, int pageSize)
-    {
-        try
+        /// <summary>
+        /// Retrieves paginated cookbooks.
+        /// </summary>
+        /// <param name="page">The page number.</param>
+        /// <param name="pageSize">The number of cookbooks per page.</param>
+        /// <returns>The paginated list of cookbooks.</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<CookBookDTO>), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        public async Task<ActionResult<List<CookBookDTO>>> GetPaginated(int page, int pageSize)
         {
+            try
+            {
             List<CookBookDTO> collections = await _cookBookService.GetPaginated(page, pageSize);
 
             return collections;
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
-        catch(Exception ex)
-        {
-            return NotFound(ex.Message);
-        }
-    }
 
-    [HttpPut]
-    [Authorize(Policy = "userPolicy")]
-    public async Task<ActionResult<CookBookDTO>> UpdateCookBook(CookBookUpdateDTO cookBookToUpdate)
-    {
-        try
+        /// <summary>
+        /// Updates a cookbook.
+        /// </summary>
+        /// <param name="cookBookToUpdate">The updated cookbook data.</param>
+        /// <returns>The updated cookbook.</returns>
+        [HttpPut]
+        [Authorize(Policy = "userPolicy")]
+        [ProducesResponseType(typeof(CookBookDTO), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        public async Task<ActionResult<CookBookDTO>> UpdateCookBook(CookBookUpdateDTO cookBookToUpdate)
+        {
+            try
+            {
+                var userId = _userService.GetMyId();
+                var cookBook = await _cookBookService.Update(cookBookToUpdate, userId);
+                return Ok(cookBook);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Deletes a cookbook by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the cookbook to delete.</param>
+        /// <returns>The deleted cookbook.</returns>
+        [HttpDelete("{id}")]
+        [Authorize(Policy = "userPolicy")]
+        [ProducesResponseType(typeof(CookBookDTO), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        public async Task<ActionResult<CookBookDTO>> Delete(Guid id)
         {
             var userId = _userService.GetMyId();
-            var cookBook = await _cookBookService.Update(cookBookToUpdate, userId);
+            CookBookDTO cookBook;
+            try
+            {
+                cookBook = await _cookBookService.Delete(id, userId);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+
             return Ok(cookBook);
         }
-        catch (Exception ex) 
-        { 
-            return BadRequest(ex.Message); 
-        }
-    }
 
-    [HttpDelete("{id}")]
-    [Authorize(Policy = "userPolicy")]
-    public async Task<ActionResult<CookBookDTO>> Delete(Guid id)
-    {
-        var userId = _userService.GetMyId();
-        CookBookDTO cookBook;
-        try
+        /// <summary>
+        /// Adds a recipe to a cookbook.
+        /// </summary>
+        /// <param name="cookBookId">The ID of the cookbook.</param>
+        /// <param name="recipeId">The ID of the recipe.</param>
+        /// <returns>The ID of the updated cookbook.</returns>
+        [HttpPut("addRecipe")]
+        [Authorize(Policy = "userPolicy")]
+        [ProducesResponseType(typeof(Guid), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        public async Task<IActionResult> AddRecipeToCookBook(Guid cookBookId, Guid recipeId)
         {
-            cookBook = await _cookBookService.Delete(id, userId);
-        }
-        catch (Exception ex)
-        {
-            return NotFound(ex.Message);
+            try
+            {
+                var userId = _userService.GetMyId();
+                var cookBook = await _cookBookService.AddRecipeToCookBook(userId, cookBookId, recipeId);
+                return Ok(cookBookId);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-        return Ok(cookBook);
-    }
-
-    [HttpPut("addRecipe")]
-    [Authorize(Policy = "userPolicy")]
-    public async Task<IActionResult> AddRecipeToCookBook(Guid cookBookId, Guid recipeId)
-    {
-        try
+        /// <summary>
+        /// Removes a recipe from a cookbook.
+        /// </summary>
+        /// <param name="cookBookId">The ID of the cookbook.</param>
+        /// <param name="recipeId">The ID of the recipe.</param>
+        /// <returns>The ID of the updated cookbook.</returns>
+        [HttpPut("removeRecipe")]
+        [Authorize(Policy = "userPolicy")]
+        [ProducesResponseType(typeof(Guid), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        public async Task<IActionResult> RemoveRecipeFromCookBook(Guid cookBookId, Guid recipeId)
         {
-            var userId = _userService.GetMyId();
-            var cookBook = await _cookBookService.AddRecipeToCookBook(userId, cookBookId, recipeId);
-            return Ok(cookBookId);
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
-    }
-
-    [HttpPut("removeRecipe")]
-    [Authorize(Policy = "userPolicy")]
-    public async Task<IActionResult> RemoveRecipeFromCookBook(Guid cookBookId, Guid recipeId)
-    {
-        try
-        {
-            var userId = _userService.GetMyId();
-            var cookBook = await _cookBookService.RemoveRecipeFromCookBook(userId, cookBookId, recipeId);
-            return Ok(cookBookId);
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
+            try
+            {
+                var userId = _userService.GetMyId();
+                var cookBook = await _cookBookService.RemoveRecipeFromCookBook(userId, cookBookId, recipeId);
+                return Ok(cookBookId);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
-
--- RecipeSharingApi/RecipeSharingApi/Controllers/RecipeController.cs
+++ RecipeSharingApi/RecipeSharingApi/Controllers/RecipeController.cs
@@ -23,6 +23,14 @@ public class RecipeController : ControllerBase
 
     //TODO: Add authorization for needed endpoints
 
+    /// <summary>
+    /// Creates a new recipe.
+    /// </summary>
+    /// <param name="recipeToCreate">The recipe data to create.</param>
+    /// <returns>The created recipe.</returns>
+    [Authorize(Policy = "userPolicy")]
+    [ProducesResponseType(typeof(RecipeDTO), 200)]
+    [ProducesResponseType(typeof(string), 400)]
     [HttpPost]
     [Authorize(Policy = "userPolicy")]
 
@@ -41,15 +49,29 @@ public class RecipeController : ControllerBase
         }
     }
 
+
+    /// <summary>
+    /// Retrieves paginated recipes.
+    /// </summary>
+    /// <param name="page">The page number.</param>
+    /// <param name="pageSize">The number of recipes per page.</param>
+    /// <returns>The paginated list of recipes.</returns>
     [HttpGet]
+    [ProducesResponseType(typeof(List<Recipe>), 200)]
     public async Task<ActionResult<List<Recipe>>> GetPaginated(int page, int pageSize)
     {
         List<Recipe> recipes = await _recipeService.GetPaginated(page, pageSize);
         return recipes;
     }
 
+    /// <summary>
+    /// Retrieves all recipes.
+    /// </summary>
+    /// <returns>The list of all recipes.</returns>
     [HttpGet]
     [Route("getAll")]
+    [ProducesResponseType(typeof(List<Recipe>), 200)]
+    [ProducesResponseType(typeof(string), 404)]
     public async Task<ActionResult<List<Recipe>>> GetAll()
     {
         try
@@ -64,9 +86,14 @@ public class RecipeController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Retrieves a specific recipe by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the recipe to retrieve.</param>
+    /// <returns>The recipe with the specified ID.</returns>
     [HttpGet("{id}")]
-    //[Authorize(AuthenticationSchemes = "Bearer")]
-    //[AllowAnonymous]
+    [ProducesResponseType(typeof(Recipe), 200)]
+    [ProducesResponseType(typeof(string), 404)]
     public async Task<ActionResult<Recipe>> Get(Guid id)
     {
         try
@@ -91,7 +118,14 @@ public class RecipeController : ControllerBase
     }
 
 
+    /// <summary>
+    /// Retrieves the nutrients for a specific recipe.
+    /// </summary>
+    /// <param name="id">The ID of the recipe.</param>
+    /// <returns>The nutrients for the specified recipe.</returns>
     [HttpGet("{id}/nutrients")]
+    [ProducesResponseType(typeof(object), 200)]
+    [ProducesResponseType(typeof(string), 404)]
     public async Task<IActionResult> GetRecipeNutrients(Guid id)
     {
         try
@@ -106,7 +140,14 @@ public class RecipeController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Updates an existing recipe.
+    /// </summary>
+    /// <param name="recipeToUpdate">The updated recipe data.</param>
+    /// <returns>The updated recipe.</returns>
     [HttpPut]
+    [ProducesResponseType(typeof(Recipe), 200)]
+    [ProducesResponseType(typeof(string), 404)]
     public async Task<ActionResult<Recipe>> Update(RecipeUpdateDTO recipeToUpdate)
     {
         try
@@ -122,7 +163,14 @@ public class RecipeController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Retrieves recipes by user ID.
+    /// </summary>
+    /// <param name="userId">The ID of the user.</param>
+    /// <returns>The list of recipes created by the specified user.</returns>
     [HttpGet("user/{userId}")]
+    [ProducesResponseType(typeof(List<Recipe>), 200)]
+    [ProducesResponseType(typeof(string), 404)]
     public async Task<ActionResult<List<Recipe>>> GetRecipesByUserId(Guid userId)
     {
         try
@@ -136,8 +184,14 @@ public class RecipeController : ControllerBase
         }
     }
 
+    // <summary>
+    /// Retrieves the recipes created by the authenticated user.
+    /// </summary>
+    /// <returns>The list of recipes created by the authenticated user.</returns>
     [HttpGet("user")]
     [Authorize(Policy = "userPolicy")]
+    [ProducesResponseType(typeof(List<Recipe>), 200)]
+    [ProducesResponseType(typeof(string), 404)]
     public async Task<ActionResult<List<Recipe>>> GetMyRecipes()
     {
         try
@@ -152,7 +206,14 @@ public class RecipeController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Retrieves recipes by cuisine ID.
+    /// </summary>
+    /// <param name="cuisineId">The ID of the cuisine.</param>
+    /// <returns>The list of recipes associated with the specified cuisine.</returns>
     [HttpGet("cuisine/{cuisineId}")]
+    [ProducesResponseType(typeof(List<Recipe>), 200)]
+    [ProducesResponseType(500)]
     public async Task<IActionResult> GetRecipesByCuisineId(Guid cuisineId)
     {
         try
@@ -166,7 +227,14 @@ public class RecipeController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Deletes a recipe by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the recipe to delete.</param>
+    /// <returns>The deleted recipe.</returns>
     [HttpDelete("{id}")]
+    [ProducesResponseType(typeof(RecipeDTO), 200)]
+    [ProducesResponseType(typeof(string), 404)]
     public async Task<ActionResult<RecipeDTO>> Delete(Guid id)
     {
         try
