885a9662fea24602c44f2090b32111c726d99193
Shefket Bylykbashi <shefketbylykbashi11@gmail.com>
2023-05-25 22:50:32 +0200

    endpoints for change user data , and change  user premmission

---
+++ RecipeSharingApi/RecipeSharingApi.Data/Models/DTOs/User/UpdateUserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeSharingApi.DataLayer.Models.DTOs.User
+{
+    public class UpdateUserRole
+    {
+
+        public Guid RoleId { get; set; }
+    }
+}
